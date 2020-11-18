bb702a7c05aaf4d287308f06f245571f72924e55
Danilo Woznica <danilowoz@gmail.com>
2020-11-18 09:09:53 +0000

    feat: improvements

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,15 @@
 const purgecss = [
   '@fullhuman/postcss-purgecss',
   {
-    // Specify the paths to all of the template files
-    content: [
-      './pages/**/*.{js,jsx,ts,tsx}',
-      './components/**/*.{js,jsx,ts,tsx}',
-    ],
-    // This is the function used to extract class names from the templates
-    defaultExtractor: (content) => {
-      // Capture as liberally as possible, including things like `h-(screen-1.5)`
-      const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
-      // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-      const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
-      return broadMatches.concat(innerMatches);
-    },
+    content: ['./components/**/*.js', './pages/**/*.js'],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
-];
+]
 module.exports = {
   plugins: [
+    'postcss-import',
     'tailwindcss',
-    process.env.NODE_ENV === 'production' ? purgecss : undefined,
-    'postcss-preset-env',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
-};
+}
